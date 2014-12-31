82c93a699e68331f2d84b4b0eee69407d85445ee
Erwan Moreau <moreaue@cs.tcd.ie>
2015-01-01 00:01:16 +0100

    fixed git update script and removed old script now obsolete

--- bin/git-update-tw-aggregator.sh
+++ bin/git-update-tw-aggregator.sh
@@ -5,15 +5,11 @@
 
 # must be run from the project dir
 
-wiki="tw-aggregator"
 
 # just in case, but things will go wrong if merging is needed
 git pull
 # run the aggregator
-bin/tw-aggregator.sh tw-sites-list.txt
-# add/delete any new tiddler
-# git add -u "$wiki"/tiddlers/*/tiddlers/
-# commit and push
+tw-community-search.sh tw-sites-list.txt
 git commit -m "automatic update" tw-community-search.html
 git push
 
