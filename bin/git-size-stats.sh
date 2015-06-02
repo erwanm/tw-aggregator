8b567c2d0a1399a772062d9d5ebbbc0569b192cf
Erwan Moreau <moreaue@cs.tcd.ie>
2015-06-03 00:02:45 +0100

    minor

--- bin/git-size-stats.sh
+++ bin/git-size-stats.sh
@@ -6,8 +6,8 @@ git rev-list master | while read rev; do
     date=$(git show -s --format=%ci $rev | cut -d " " -f 1)
     size=$(git ls-tree -r $rev -l "$file" | cut -d " " -f 4 | sed "s/$file//g")
     if [ ! -z "$size" ]; then
-	echo -e "$date\t$size"
+	echo -e "$rev\t$date\t$size"
     fi
-done | sort -u +0 -1
-
+done
+echo "info: pipe the output with sort -u +1 -2 for daily size." 1>&2
 
