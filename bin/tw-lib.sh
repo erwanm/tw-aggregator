8c9ae06d47930348cae1dda61b2072ffb62a3e61
Erwan Moreau <moreaue@cs.tcd.ie>
2015-11-17 01:54:40 +0000

    bug fix: space in wiki id messes up wikiid tag on regular TWCS tiddlers

--- bin/tw-lib.sh
+++ bin/tw-lib.sh
@@ -231,7 +231,7 @@ function cloneAsTWCSTiddler {
     echo "title: $newTitle" >"$targetTiddler"
     printTiddlerFields "$sourceTiddlerFile" "title tags $excludeFields" "$firstBlankLineNo" >>"$targetTiddler"
     oldTags=$(extractField "tags" "$sourceTiddlerFile" "$firstBlankLineNo")
-    writeTagsIfNotSystem "$tagsListFile" "$wikiId $additionalTags" "$oldTags" >>"$targetTiddler"
+    writeTagsIfNotSystem "$tagsListFile" "[[$wikiId]] $additionalTags" "$oldTags" >>"$targetTiddler"
     echo "source-wiki-id: $wikiId" >>"$targetTiddler" # store custom fields in order to recompute the original address
     # url-encode the title, in case it contains characters like # (see github bug #24)
     # also keep the non-encoded title (named source-tiddler-title-as-text), to display it in a user-friendly readable text
